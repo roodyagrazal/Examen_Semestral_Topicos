83a40485c0295ffe68d45b9ef5a7a77e46aab189
roody agrazal <roody@agrazal>
2020-07-29 10:19:17 -0500

    Added Csv file

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from core import ProcessData
 
+class Procees_Data(ProcessData):
+    pass
+
 if __name__ == "__main__":
-    ProcessData.run()
+    ctx = {}
+    process = ProcessData(**ctx)
+    process.run()
--- core/exam_core.py
+++ core/exam_core.py
@@ -1,7 +1,14 @@
+import os
+
 class ProcessData:
 
     def __init__(self, *args, **kwargs):
         self.pwd = os.path.dirname(os.path.realpath(__file__))
     
+    def get_file_info(self):
+        file = open("core/info.csv", "r")
+        with file as csv:
+            print(csv.readlines())
+
     def run(self):
-        print(self.pwd)
+        self.get_file_info()
