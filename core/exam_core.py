1f44683498710c44394d74c0f8fa8af1ad64200f
roody agrazal <roody@agrazal>
2020-07-29 19:37:33 -0500

    WIP

--- core/exam_core.py
+++ core/exam_core.py
@@ -1,23 +1,27 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import pytesseract
+
 
 
 class ProcessData:
 
     def __init__(self, *args, **kwargs):
         self.pwd = os.path.dirname(os.path.realpath(__file__))
-    
-    def get_file_info(self):
-        file = open("core/info.csv", "r")
-        with file as csv:
-            print(csv.readlines())
+        self.csv_14_july = "core/info2.csv"
+        self.csv_12_july = "core/info.csv"
+        self.data_raw = []
 
     def read_csv_panda(self):
-        df = pd.read_csv("core/info.csv")
-        data = df.iloc[:,0:3].values
-        print(data)
+        df = pd.read_csv(self.csv_14_july)
+        self.data_raw = df.iloc[:,0:3].values
+
+    def data_analisis(self):
+        print(self.data_raw)
 
     def run(self):
-        self.get_file_info()
         self.read_csv_panda()
+        self.data_analisis()
+        
