9d9773b2bb1e74813387454e684f40f7e876a112
roody agrazal <roody@agrazal>
2020-07-31 15:08:32 -0500

    Added travis Scott

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,14 @@ WORKDIR /src
 COPY ./requirements.txt /src/requirements.txt
 RUN pip install -r requirements.txt
 
+RUN apt-get update \
+    && apt-get install tesseract-ocr -y \
+    python3 \
+    python-setuptools \
+    python3-pip \
+    && apt-get clean \
+    && apt-get autoremove
+
 COPY ./app.py /src/app.py
 COPY ./core /src/core
 
--- core/exam_core.py
+++ core/exam_core.py
@@ -2,8 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
 import pytesseract
+from PIL import Image
 
+
+pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
 
 
 class ProcessData:
@@ -13,15 +17,26 @@ class ProcessData:
         self.csv_14_july = "core/info2.csv"
         self.csv_12_july = "core/info.csv"
         self.data_raw = []
+        self.im = Image.open('core/12jul.jpeg')
 
     def read_csv_panda(self):
         df = pd.read_csv(self.csv_14_july)
         self.data_raw = df.iloc[:,0:3].values
 
     def data_analisis(self):
-        print(self.data_raw)
+        #print(pytesseract.image_to_string(self.im))
+        data_raw = pytesseract.image_to_string(self.im)
+        data_split = data_raw.split()
+        i = 13
+        data_dict = {}
+        #This creates a dictionary with the data from the image.
+        for data in data_split[13:130]:
+            temp = data
+            data_dict[data + " " + temp] = i
+        print(data_dict)
 
     def run(self):
         self.read_csv_panda()
         self.data_analisis()
+        
         
